d39b9f866297e575a1af708c1bbf41d5b92bb7ca
R.A. Lucas <richard@richardalucas.com>
2014-06-28 18:39:16 -0600

    initial commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+$(function() {
+
+    $('section[data-type="background"]').each(function(){
+        var $bgObj = $(this);
+
+        $(window).scroll(function() {
+            var yPos = -($(window).scrollTop() / $bgObj.data('speed'));
+
+            var coords = '50% '+ yPos + 'px';
+
+            $bgObj.css({ backgroundPosition: coords });
+        });
+    });
+
+});
---
+++ todo.md
@@ -0,0 +1,11 @@
+To-do for web site for freelance web programming
+---
+
+- Parallax/scrolling effect
+- Responsive
+- Uses Semantic UI
+- Uses a framework --- thinking Backbone.js
+- Runs on Node.js
+- Link to twitter module
+- Single page (all links on top bar just move the scroll)
+- Utilizes Sendgrid for email
